f2fd20a0f5cca485e7b31e8247b14aec681dc8b3
shifinmalik <137679921+shifnmalik@users.noreply.github.com>
2024-07-15 06:25:38 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,14 +7,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Home />} />
-         
-        </Route>
         <Route path='/E-Cart' element={<Layout />}>
           <Route index element={<Home />} />
          
         </Route>
+       
         <Route path='/:slug' element={<Detail />} />
       </Routes>
     </BrowserRouter>
